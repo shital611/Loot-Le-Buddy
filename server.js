4191c0947e23bee7f89db6c95863125ce431be9c
shital <shitalsonawane51120@gmail.com>
2022-08-01 17:45:26 +0530

    port change

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
+const port=process.env.PORT ||3000
 
 const employeeController = require('./controllers/employeeController');
 
@@ -25,8 +26,8 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+app.listen(port, () => {
+    console.log(`Express server started at port : ${port}`);
 });
 
 app.use('/pool', employeeController);
